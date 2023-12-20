ab153578791802faea630cb8772de318538beae5
noppadon <noppadonsangthongw@gmail.com>
2023-12-21 01:51:15 +0700

    add: feat new post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,79 @@
+import React, { useState } from "react";
+
 function Posts() {
+  const [post, setPost] = useState([]);
+  const [postText, setPostText] = useState("");
+  const [countLikes, setCountLikes] = useState([]);
+
+  const handlePostTextChange = (event) => {
+    setPostText(event.target.value);
+  };
+
+  const addPost = (event) => {
+    event.preventDefault();
+    const newPost = [...post];
+    newPost.push(postText);
+    setPost(newPost);
+    setCountLikes([...countLikes, 0]);
+    setPostText("");
+  };
+
+  const addLike = (likeIndex) => {
+    const newLikes = [...countLikes];
+    newLikes[likeIndex] += 1;
+    setCountLikes(newLikes);
+  };
+
+  const addDisLike = (likeIndex) => {
+    const newLikes = [...countLikes];
+    newLikes[likeIndex] = Math.max(0, newLikes[likeIndex] - 1);
+    setCountLikes(newLikes);
+  };
+
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+    <div className="app-wrapper">
+      <h1 className="app-title">Posts</h1>
+      <div className="message-input-container">
+        <label>
+          <input
+            id="message-text"
+            name="message-text"
+            type="text"
+            value={postText}
+            onChange={handlePostTextChange}
+            placeholder="Enter Post here"
+          />
+        </label>
+        <button onClick={addPost} className="submit-message-button">
+          Submit
+        </button>
+      </div>
+      <div className="post-list">
+        {post.map((item, index) => {
+          return (
+            <div className="post-item" key={index}>
+              <div className="post-header">
+                <h2>Post Title #{index + 1}</h2>
+                <div className="post-social-media-stats">
+                  <span className="stats-topic">Likes: </span>
+                  <span className="post-likes">{countLikes[index]}</span>
+                </div>
+              </div>
+              <p className="post-content">{item}</p>
+              <div className="post-actions">
+                <button className="like-button" onClick={() => addLike(index)}>
+                  Like
+                </button>
+                <button
+                  className="dislike-button"
+                  onClick={() => addDisLike(index)}
+                >
+                  Dislike
+                </button>
+              </div>
             </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
